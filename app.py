07cf60d16e9ef9aa82a9bd0568e4c468ffdbbb5f
AlexMateo172 <1725110135@utectulancingo.edu.mx>
2026-09-23 07:38:36 +0000

    Funcional

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import web
 
 urls = (
     '/', 'controllers.index.Index',
-    '/agregar', 'controllers.index.Agregar',
-    '/atender', 'controllers.index.Atender'
+    '/agregar', 'controllers.agregar.Agregar',
+    '/atender', 'controllers.atender.Atender'
 )
 
 app = web.application(urls, globals())
-
-render = web.template.render('views')
 
 if __name__ == "__main__":
     app.run()
